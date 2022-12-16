810a102b6b246699e4ed7d6b73ecfe4aacaed6b0
Kelvin Brinham <kelvinbrinham@gmail.com>
2022-12-16 17:29:32 +0000

    ok

--- Plotting_time_series.py
+++ Plotting_time_series.py
@@ -40,6 +40,10 @@ date_one_month_forward_lst = []
 date_three_months_forward_lst = []
 date_six_months_forward_lst = []
 
+three_month_high_RP_date_lst = list(Neste_and_SXXP_HIGH_DATA_df['Date'])
+three_month_high_RP_lst = list(Neste_and_SXXP_HIGH_DATA_df['RP_3_month_high'])
+
+
 
 for i in range(len(date_lst)):
     if date_lst[i] in rolling_3_month_high_RP_date_st:
@@ -53,11 +57,12 @@ for i in range(len(date_lst)):
 fig, ax = plt.subplots(figsize=(10,7))
 
 RP, = ax.plot(date_lst, RP_lst, label = 'RP')
-RP_3_month_high, = ax.plot(date_lst, rolling_3_month_high_RP_lst, label = 'RP 3-month High')
+RP_3_month_high_rolling, = ax.plot(date_lst, rolling_3_month_high_RP_lst, label = 'RP Rolling 3-month High')
 
 RP_1_month_after_3_month_high, = ax.plot(date_one_month_forward_lst, RP_after_high_dict['one_month'], label = 'RP 1 Month After 3-month High')
 RP_3_month_after_3_month_high, = ax.plot(date_three_months_forward_lst, RP_after_high_dict['three_months'], label = 'RP 3 Months After 3-month High')
 RP_6_month_after_3_month_high, = ax.plot(date_six_months_forward_lst, RP_after_high_dict['six_months'], label = 'RP 6 Months After 3-month High')
+RP_3_month_high, = ax.plot(three_month_high_RP_date_lst, three_month_high_RP_lst, label = 'RP 3-month High')
 
 # RP_1_month_after_3_month_high, = ax.plot(rolling_3_month_high_RP_date_no_dupli_lst[1:], RP_after_high_dict['one_month'], label = '1 Month RP')
 # RP_3_month_after_3_month_high, = ax.plot(rolling_3_month_high_RP_date_no_dupli_lst[2:], RP_after_high_dict['three_months'], label = '3 Month RP')
@@ -67,13 +72,13 @@ RP_6_month_after_3_month_high, = ax.plot(date_six_months_forward_lst, RP_after_h
 
 legend = plt.legend(loc='upper left')
 
-RP_leg, RP_3_month_high_leg, RP_1_month_after_3_month_high_leg, RP_3_month_after_3_month_high_leg, RP_6_month_after_3_month_high_leg = legend.get_lines()
+RP_leg, RP_3_month_high_rolling_leg, RP_1_month_after_3_month_high_leg, RP_3_month_after_3_month_high_leg, RP_6_month_after_3_month_high_leg, RP_3_month_high_leg = legend.get_lines()
 
 RP_leg.set_picker(True)
 RP_leg.set_pickradius(10)
 
-RP_3_month_high_leg.set_picker(True)
-RP_3_month_high_leg.set_pickradius(10)
+RP_3_month_high_rolling_leg.set_picker(True)
+RP_3_month_high_rolling_leg.set_pickradius(10)
 
 RP_1_month_after_3_month_high_leg.set_picker(True)
 RP_1_month_after_3_month_high_leg.set_pickradius(10)
@@ -84,12 +89,16 @@ RP_3_month_after_3_month_high_leg.set_pickradius(10)
 RP_6_month_after_3_month_high_leg.set_picker(True)
 RP_6_month_after_3_month_high_leg.set_pickradius(10)
 
+RP_3_month_high_leg.set_picker(True)
+RP_3_month_high_leg.set_pickradius(10)
+
 graphs_dict = {}
 graphs_dict[RP_leg] = RP
-graphs_dict[RP_3_month_high_leg] = RP_3_month_high
+graphs_dict[RP_3_month_high_rolling_leg] = RP_3_month_high_rolling
 graphs_dict[RP_1_month_after_3_month_high_leg] = RP_1_month_after_3_month_high
 graphs_dict[RP_3_month_after_3_month_high_leg] = RP_3_month_after_3_month_high
 graphs_dict[RP_6_month_after_3_month_high_leg] = RP_6_month_after_3_month_high
+graphs_dict[RP_3_month_high_leg] = RP_3_month_high
 
 def on_pick(event):
     legend_ = event.artist
@@ -105,6 +114,6 @@ plt.connect('pick_event', on_pick)
 
 ax.set_title('Time Series Data')
 
-# plt.savefig('Plot_examples/Time_Series_Data.png', dpi = 800)
+# plt.savefig('Plot_examples/Time_Series_Data_example.png', dpi = 800)
 
-# plt.show()
+plt.show()
